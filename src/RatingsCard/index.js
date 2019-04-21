f4768422561978e7342b83132101eb9e15671d36
Matt Crouch <matt@mattcrouch.net>
2019-04-21 10:19:23 +0100

    Shimmy effect

--- src/RatingsCard/index.js
+++ src/RatingsCard/index.js
@@ -2,12 +2,30 @@ import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
 import "./styles.css";
 
+// Calculate the tilt based on the cursor position on screen rather than the card
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1
+];
+
+// Apply values to transform property
+const trans = (x, y, s) =>
+  `perspective(1000px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+// Flipping interpolation
 const inverseOpacity = o => 1 - o;
 const inverseTransform = t => `${t} rotateY(180deg)`;
 
 export const RatingsCard = ({ image }) => {
   const [selected, setSelected] = useState(false);
 
+  // Card shimmy
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1]
+  }));
+
+  // Flipping
   const { opacity, transform } = useSpring({
     config: {
       friction: 22,
@@ -18,7 +36,13 @@ export const RatingsCard = ({ image }) => {
   });
 
   return (
-    <div onClick={() => setSelected(!selected)} className="RatingsCard">
+    <animated.div
+      onClick={() => setSelected(!selected)}
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      className="RatingsCard"
+      style={{ transform: !selected && props.xys.interpolate(trans) }}
+    >
       <animated.div
         className="RatingsCard__front"
         style={{
@@ -36,7 +60,7 @@ export const RatingsCard = ({ image }) => {
       >
         BACK
       </animated.div>
-    </div>
+    </animated.div>
   );
 };
 
