a10aba929d332dd15781583463b0e53c872b160c
Matt Crouch <matt@mattcrouch.net>
2019-04-22 21:06:26 +0100

    White colour on unselected values

--- src/Star/index.js
+++ src/Star/index.js
@@ -26,7 +26,7 @@ export class Star extends Component {
             id="Star"
             stroke="#FFFFFF"
             strokeWidth="5"
-            fill={this.props.active ? "#F9BE3E" : undefined}
+            fill={this.props.active ? "#F9BE3E" : "#FFFFFF"}
             points="51 83.1013765 21.6107374 93.4508497 22.3718897 62.3018369 3.44717419 37.5491503 33.3068548 28.6474749 51 3 68.6931452 28.6474749 98.5528258 37.5491503 79.6281103 62.3018369 80.3892626 93.4508497"
           />
         </g>
