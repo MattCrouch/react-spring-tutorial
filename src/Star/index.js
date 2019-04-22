5686e4ec7737b7266a13296fb21c6ac9ffd86ac1
Matt Crouch <matt@mattcrouch.net>
2019-04-22 21:16:16 +0100

    Star as button

--- src/Star/index.js
+++ src/Star/index.js
@@ -5,32 +5,33 @@ import "./styles.css";
 export class Star extends Component {
   render() {
     return (
-      <svg
-        className="Star"
-        style={this.props.style}
-        onClick={this.props.onClick}
-        onMouseEnter={this.props.onMouseEnter}
-        width="102px"
-        height="96px"
-        viewBox="0 0 102 96"
-        version="1.1"
-      >
-        <g
-          stroke="none"
-          strokeWidth="1"
-          fill="none"
-          fillRule="evenodd"
-          strokeLinejoin="round"
+      <button className="Star" style={this.props.style}>
+        <svg
+          className="Star__icon"
+          onClick={this.props.onClick}
+          onMouseEnter={this.props.onMouseEnter}
+          width="102px"
+          height="96px"
+          viewBox="0 0 102 96"
+          version="1.1"
         >
-          <polygon
-            id="Star"
-            stroke="#FFFFFF"
-            strokeWidth="5"
-            fill={this.props.active ? "#F9BE3E" : "#FFFFFF"}
-            points="51 83.1013765 21.6107374 93.4508497 22.3718897 62.3018369 3.44717419 37.5491503 33.3068548 28.6474749 51 3 68.6931452 28.6474749 98.5528258 37.5491503 79.6281103 62.3018369 80.3892626 93.4508497"
-          />
-        </g>
-      </svg>
+          <g
+            stroke="none"
+            strokeWidth="1"
+            fill="none"
+            fillRule="evenodd"
+            strokeLinejoin="round"
+          >
+            <polygon
+              id="Star"
+              stroke="#FFFFFF"
+              strokeWidth="5"
+              fill={this.props.active ? "#F9BE3E" : "#FFFFFF"}
+              points="51 83.1013765 21.6107374 93.4508497 22.3718897 62.3018369 3.44717419 37.5491503 33.3068548 28.6474749 51 3 68.6931452 28.6474749 98.5528258 37.5491503 79.6281103 62.3018369 80.3892626 93.4508497"
+            />
+          </g>
+        </svg>
+      </button>
     );
   }
 }
